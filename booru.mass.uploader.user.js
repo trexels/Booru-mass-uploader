237098db918d5710b9be80dafb6f325a12d71af4
ProximaNova <jaretrudd@gmail.com>
2015-11-25 12:32:16 -0700

    Update booru.mass.uploader.user.js

--- booru.mass.uploader.user.js
+++ booru.mass.uploader.user.js
@@ -6,6 +6,7 @@
 // @include	http://*.booru.org/index.php*
 // @include 	http://gelbooru.com/index.php*
 // @include	http://safebooru.org/index.php*
+// @include	http://rule34.xxx/index.php*
 // @grant 		none 
 // @noframes
 // ==/UserScript==
