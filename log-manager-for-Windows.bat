d50db3bf66fd6f8fda1b2eb335bd58119b6dcc56
ProximaNova <jaretrudd@gmail.com>
2016-09-13 12:56:43 -0600

    Update log-manager-for-Windows.bat

--- log-manager-for-Windows.bat
+++ log-manager-for-Windows.bat
@@ -1,5 +1,6 @@
 :: Use the following command in Vim on the log:
 :: %s/^.* Started uploading.*\.$//ge | %s/^.* Finished uploading.*total\.$//ge | %s/^.* Image \(.* was successfully uploaded\.$\)\@=/del "/ge | %s/.* Couldn't upload \(.*: image already exists here\.\)\@=/del "/ge | %s/: image already exists here\.$/"/ge | %s/ was successfully uploaded\.$/"/ge | %s/.* Couldn't upload \(.*: image too big? too small? corrupted?\.\)\@=/move "/ge | %s/: image too big? too small? corrupted?\./" upload_failure_dimensions/ge | %s/.* Couldn't upload \(.*: undefined\.$\)\@=/move "/ge | %s/: undefined\.$/" upload_failure_undefined/ge | g/^$/d
+:: old version: %s/^.* Image \(.* was successfully uploaded\.$\)\@=/del "/ge | %s/.* Couldn't upload \(.*: image already exists here\.\)\@=/del "/ge | %s/: image already exists here\.$/"/ge | %s/ was successfully uploaded\.$/"/ge | %s/.* Couldn't upload \(.*: image too big? too small? corrupted?\.\)\@=/move "/ge | %s/: image too big? too small? corrupted?\./" upload_failure_dimensions/ge | %s/.* Couldn't upload \(.*: undefined\.$\)\@=/move "/ge | %s/: undefined\.$/" upload_failure_undefined/ge | g/^$/d
 
 If Not Exist "upload_failure_dimensions\" (
     mkdir upload_failure_dimensions
